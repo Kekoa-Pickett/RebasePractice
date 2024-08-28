b49c34ab86b379ade40230f3cec25b34372cb76a
Kekoa-Pickett <103221420+Kekoa-Pickett@users.noreply.github.com>
2024-08-28 13:33:57 -0700

    Update Odd1-20.py

--- Odd1-20.py
+++ Odd1-20.py
@@ -1,10 +1,8 @@
 print("Hello world")
 j = 0
-while j < 20:
-    print ("J > 20")
-    j += 2
 for i in range(20):
     if (i % 2 > 0):
         print("odd")
+print("I need this to show")
 print("This should also show")
 abc = 10
